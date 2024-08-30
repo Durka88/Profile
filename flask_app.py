797e8bb4c2c4836af1cd0171fc997cb1d85a381c
Durka Devi <Durkad88@outlook.com>
2024-08-30 10:02:31 +0000

    First cut at the login page

--- flask_app.py
+++ flask_app.py
@@ -34,3 +34,9 @@ def index():
     db.session.add(comment)
     db.session.commit()
     return redirect(url_for('index'))
+
+
+#to display login
+@app.route("/login/")
+def login():
+    return render_template("login_page.html")
